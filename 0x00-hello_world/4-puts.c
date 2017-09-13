d6308715b3480800111d5e25d3ed47731cb63c7e
KevinClauson <kevinclauson99@gmail.com>
2017-09-13 20:30:04 +0000

    changed style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
+/**
+ * 4-puts.c - Prints a message to the terminal.
+ * @parameterx: Description of parameter x
+ *
+ * Description: Longer description of the function)?
+ * section header: Section description)*
+ * Return: Description of the returned value
+ */
 #include <stdio.h>
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
