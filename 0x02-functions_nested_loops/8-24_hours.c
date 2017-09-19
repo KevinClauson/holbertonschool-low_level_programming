204ef2904111843e50af06b4a7009933e2e8bc3e
KevinClauson <kevinclauson99@gmail.com>
2017-09-19 17:08:47 +0000

    put in spaces

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int i, j, h1, h2, m1, m2;
 
-	for(i = 0; i < 24; ++i)
+	for (i = 0; i < 24; ++i)
 	{
-		for(j = 0; j < 60; ++j)
+		for (j = 0; j < 60; ++j)
 		{
 			h1 = i / 10 + '0';
 			h2 = i % 10 + '0';
