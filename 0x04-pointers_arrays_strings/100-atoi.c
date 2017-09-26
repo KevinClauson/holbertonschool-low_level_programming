7be4d8312197eb95458d9c3b99cbcdc6d97363a5
KevinClauson <kevinclauson99@gmail.com>
2017-09-26 14:53:16 +0000

    print first number in string

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _atoi -  convert a string to an integer.
+ * @s: the string to be converted.
+ * Return: void.
+ */
+int _atoi(char *s)
+{
+	int num, i, is_num, one;
+
+	num = is_num = 0;
+	one = 1;
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if ((s[i] < '0' || s[i] > '9') && is_num == 1)
+			break;
+		if (s[i] == '-' && is_num == 0)
+		{
+			one *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			is_num = 1;
+			num *= 10;
+			num += s[i] - '0';
+		}
+	}
+	num *= one;
+	return (num);
+}
