6473a209988c00e225d34df0f01625a8b2828bb3
KevinClauson <kevinclauson99@gmail.com>
2017-09-30 06:43:48 +0000

    test

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -24,8 +24,7 @@ int test_size(char *n1, int l1, char *n2, int l2, char *r, int s)
 {
 	int i, j, temp, carry, k;
 
-	carry = 0;
-	r[s - 1] = '\0';
+	carry = 0; r[s - 1] = '\0';
 	for (i = l1 - 1, j = l2 - 1, k = s - 2; i >= 0 || j >= 0; --i, --j, --k)
 	{
 		if (i >= 0 && j >= 0)
@@ -41,27 +40,23 @@ int test_size(char *n1, int l1, char *n2, int l2, char *r, int s)
 		}
 		else if (i >= 0)
 		{
-			temp = (n1[i] - '0') + carry;
-			carry = 0;
+			temp = (n1[i] - '0') + carry; carry = 0;
 			if (temp > 9)
 			{
 				temp %= 10;
 				carry = 1;
 			}
-			r[k] = temp + '0';
-			temp = 0;
+			r[k] = temp + '0'; temp = 0;
 		}
 		else if (j >= 0)
 		{
-			temp = (n2[j] - '0') + carry;
-			carry = 0;
+			temp = (n2[j] - '0') + carry; carry = 0;
 			if (temp > 9)
 			{
 				temp %= 10;
 				carry = 1;
 			}
-			r[k] = temp + '0';
-			temp = 0;
+			r[k] = temp + '0'; temp = 0;
 		}
 		else if (carry == 1)
 		{
@@ -80,6 +75,12 @@ int test_size(char *n1, int l1, char *n2, int l2, char *r, int s)
 	else if (carry == 1 && k == 0)
 	{
 		r[k] = '1';
+		return (1);
+	}
+	else if(k > 0)
+	{
+		k += 1;
+		move_str(r, k, s);
 		return (1);
 	}
 	else
