9ae5cbde9909ad9aced3e55ee6f634bc00f92278
KevinClauson <kevinclauson99@gmail.com>
2017-10-11 00:21:39 +0000

    add check for null after malloc

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -22,8 +22,8 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *) malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
-			for ( ;i < 0; --i)
-				free (arr[i]);
+			for ( ; i >= 0; --i)
+				free(arr[i]);
 			free(arr);
 			return (NULL);
 		}
