d1c196a9b672f6c1787448ae22cc050ebd18ac4d
KevinClauson <kevinclauson99@gmail.com>
2017-10-13 07:03:43 +0000

    fill with zeros

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -9,11 +9,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
 	return (ptr);
 }
