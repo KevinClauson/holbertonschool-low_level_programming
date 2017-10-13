47ec8edbe1d401920292d5ab67cd3199d9367b0a
KevinClauson <kevinclauson99@gmail.com>
2017-10-13 06:35:11 +0000

    edge case

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -44,8 +44,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str = malloc(len2 + 1 * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < len1; ++i)
-		str[i] = s1[i];
 	if (len2 >= n)
 	{
 		for (j = 0; j < n; ++j, ++i)
@@ -54,7 +52,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		for(j = 0; str[j] != '\0'; ++j, ++i)
+		for(j = 0; s2[j] != '\0'; ++j, ++i)
 			str[i] = s2[j];
 		str[i] = '\0';
 	}
