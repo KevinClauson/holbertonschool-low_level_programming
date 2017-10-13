a2a47db2c2e01e8e1a33e8e312868286f8e2eefd
KevinClauson <kevinclauson99@gmail.com>
2017-10-13 07:13:56 +0000

    change logic in for loop

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -29,24 +29,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1, len2, i, j;
 
 	if (s1 == NULL)
-		len1 = 0;
-	else
-		len1 = _strlen(s1);
+		s1 = "";
 	if (s2 == NULL)
-		len2 = 0;
-	else
-		len2 = _strlen(s2);
-	if (len1 == 0 && len2 == 0)
-		return (NULL);
-	if (len2 >= n)
-		str = malloc(len1 + n + 1 * sizeof(char));
-	else
-		str = malloc(len2 + 1 * sizeof(char));
+		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n >= len2)
+		n = len2;
+	str = malloc(len1 + n + 1 * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < len1; ++i)
 		str[i] = s1[i];
-	for (j = 0; j < n && s2[j] != '\0'; ++j, ++i)
+	for (j = 0; j < n; ++j, ++i)
 		str[i] = s2[j];
 	str[i] = '\0';
 	return (str);
