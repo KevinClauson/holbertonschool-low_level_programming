b55937af6de9bbbb37fe1372485094df7a0416d2
KevinClauson <kevinclauson99@gmail.com>
2017-10-13 06:11:10 +0000

    project 11

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _strlen - get length of string.
+ * @str: string.
+ * Return: length.
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		++i;
+	}
+	return (i);
+}
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: strging 1.
+ * @s2: string 2.
+ * @n: byets of s2 to include.
+ * Return: pointer to a string or NULL.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int len1, len2, i, j;
+
+	len1 = _strlen(s1);
+	if (s2 == NULL)
+		len2 = 0;
+	else
+		len2 = _strlen(s2);
+
+	if (len2 >= n)
+		str = malloc(len1 + n + 1 * sizeof(char));
+	else
+		str = malloc(len2 + 1 * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len1; ++i)
+		str[i] = s1[i];
+	for (j = 0; j < n && s2[j] != '\0'; ++j, ++i)
+		str[i] = s2[j];
+	str[i] = '\0';
+
+	return (str);
+}
---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements in allocated array.
+ * @size: size of the bytes.
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
