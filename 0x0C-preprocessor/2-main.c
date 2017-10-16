31eb3ff6e3ce563a904aaaf11bdf90b5ebfd81b9
KevinClauson <kevinclauson99@gmail.com>
2017-10-16 20:02:23 +0000

    add comments

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main -  prints the name of the file it was compiled from.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("%s\n", __BASE_FILE__);
