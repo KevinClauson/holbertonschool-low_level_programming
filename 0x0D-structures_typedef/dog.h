5e161c779bcb056b42056fda7f6cf72a91b2f5c4
KevinClauson <kevinclauson99@gmail.com>
2017-10-16 22:45:33 +0000

    add comments

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,8 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 typedef struct dog dog;
-
-struct dog {
+/**
+ * struct dog - dog
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: name of owner.
+ *
+ * Description: use to store dogs.
+ */
+struct dog
+{
 	char   *name;
 	float  age;
 	char   *owner;
