705a021c8507fccc940204a10299ba5071089df6
KevinClauson <kevinclauson99@gmail.com>
2017-10-19 02:26:04 +0000

    style edits

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -2,7 +2,7 @@
 /**
  * get_op_func - find the correct function to use.
  * @s: the string that holds the operator.
- * Result: return a pointer to the function that coincies with the operator.
+ * Return: return a pointer to the function that coincies with the operator.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -17,9 +17,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while(ops[i].op != NULL)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(ops[i].op,s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		++i;
 	}
