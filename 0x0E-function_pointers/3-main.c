5d4f948c615e48a2faf0a902c1bebbd4583798a5
KevinClauson <kevinclauson99@gmail.com>
2017-10-19 01:34:02 +0000

    edit style

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -7,28 +7,28 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-        char *operator;
+	int n1, n2;
+        char *op;
 
 	if (argc < 4)
 	{
 		printf("Error\n");
 		exit (98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
 	operator = argv[2];
-	if (get_op_func(operator) == NULL)
+	if (get_op_func(op) == NULL)
 	{
 		printf("Error\n");
 		exit (99);
 	}
-	if (num2 == 0 && (strcmp(operator,"/") == 0 || strcmp(operator,"%") == 0))
+	if (n2 == 0 && (strcmp(op,"/") == 0 || strcmp(op, "%") == 0))
 	{
 		printf("Error\n");
 		exit (100);
 	}
-	printf("%d\n", (get_op_func(operator)(num1, num2)));
+	printf("%d\n", (get_op_func(op)(n1, n2)));
 
 	return (0);
 }
