cb35c760341614dcb30cbad6584e91bdeac5d3b8
KevinClauson <kevinclauson99@gmail.com>
2017-10-21 20:43:59 +0000

    style edits

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
 /**
  * print_strings - prints strings, followed by a new line.
- * @separator
- * Return: Always 0.
+ * @separator: the string that will sparate each input
+ * @n: the number of arguments.
+ * Return: nothing.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -10,7 +11,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
+	{
 		printf("%s", va_arg(args, char *));
 		if (separator && (i < n - 1))
 			printf("%s", separator);
