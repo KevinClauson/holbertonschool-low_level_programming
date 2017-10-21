2856ed8571e6f5a6767bfba292ab1a83439efdb9
KevinClauson <kevinclauson99@gmail.com>
2017-10-21 20:32:50 +0000

    style edits

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -14,9 +14,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 	if (args == 0)
 		return (0);
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-	}
 	va_end(args);
 	return (sum);
 }
