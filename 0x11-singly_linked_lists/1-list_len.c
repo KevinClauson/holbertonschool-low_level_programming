05fbf6e20f4aa72f2f0cad9c5ba164a9a7859582
KevinClauson <kevinclauson99@gmail.com>
2017-10-26 22:18:44 +0000

    get number of elements and insert new head

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list..
+ * @h: a list of list_t.
+ * Return: Always 0.
+ */
+size_t list_len(const list_t *h)
+{
+	int cnt;
+
+	cnt = 0;
+	while(h != NULL)
+	{
+		++cnt;
+		h = h->next;
+	}
+	return(cnt);
+}
---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * add_node -  adds a new node at the beginning of a list_t list.
+ * @head: a pointer to a list of pointers.
+ * @str: string to be copied into the new node.
+ * Return: he address of the new element, or NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head;
+	new_head = malloc(sizeof(list_t));
+	new_head->str = strdup(str);
+	new_head->len = strlen(str);
+	new_head->next = *head;
+	*head = new_head;
+	return (*head);
+}
