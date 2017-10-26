a325e331c04d2912aa975804cdfc1952330535b2
KevinClauson <kevinclauson99@gmail.com>
2017-10-26 22:24:57 +0000

    add checks for NULL case

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -9,7 +9,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head;
 
+	if (head == NULL)
+		return (NULL);
+
 	new_head = malloc(sizeof(list_t));
+	if (new_head == NULL)
+		return (NULL);
 	new_head->str = strdup(str);
 	new_head->len = strlen(str);
 	new_head->next = *head;
