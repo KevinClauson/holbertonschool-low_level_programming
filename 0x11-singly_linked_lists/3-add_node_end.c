d03c1fcfcf3f8ec50128e6f016acec05bebdb760
KevinClauson <kevinclauson99@gmail.com>
2017-10-27 01:55:05 +0000

    add while loop

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -7,20 +7,22 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_head;
+	list_t *new_end, *temp;
 
-	if (head == NULL)
+	new_end = malloc(sizeof(list_t));
+	if (new_end == NULL)
 		return (NULL);
-
-	new_head = malloc(sizeof(list_t));
-	if (new_head == NULL)
-		return (NULL);
-	if (str)
+	new_end->str = strdup(str);
+	new_end->len = strlen(str);
+	new_end->next = NULL;
+	temp = *head;
+	if(*head == NULL)
 	{
-		new_head->str = strdup(str);
-		new_head->len = strlen(str);
+		*head = new_end;
+		return (new_end);
 	}
-	new_head->next = *head;
-	*head = new_head;
-	return (*head);
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_end;
+	return (new_end);
 }
