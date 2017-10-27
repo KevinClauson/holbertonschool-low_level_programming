6d5c96a58a865419b978bdb7ea7b3a82a675c802
KevinClauson <kevinclauson99@gmail.com>
2017-10-27 02:28:52 +0000

    extra credit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * quick_p - print before main function.
+ * we use __attribute__((constructor)) in the header to do this.
+ * Return: nothing.
+ */
+void quick_p(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
