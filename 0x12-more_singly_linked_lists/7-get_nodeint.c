ed7aecb848182c374d7fa9fcc99ad05f74728044
KevinClauson <kevinclauson99@gmail.com>
2017-10-31 19:10:28 +0000

    check for no such index

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i;
 
-	if(head == NULL)
+	if (head == NULL)
 		return (NULL);
 	temp = head;
 	for (i = 0; temp->next != NULL && i < index; ++i)
 	{
 		temp = temp->next;
 	}
-	return (temp);
+	if (i == index)
+		return (temp);
+	return (NULL);
 }
