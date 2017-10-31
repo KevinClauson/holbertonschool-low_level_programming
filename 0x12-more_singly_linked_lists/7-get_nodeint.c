d25cc57eb783c7abbf7df1aa50575a372e5f97e7
KevinClauson <kevinclauson99@gmail.com>
2017-10-31 19:08:13 +0000

    check for null

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int i;
 
+	if(head == NULL)
+		return (NULL);
 	temp = head;
 	for (i = 0; temp->next != NULL && i < index; ++i)
 	{
