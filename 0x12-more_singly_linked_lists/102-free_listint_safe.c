514708ba9a685faf78b302470b90b7da1db2c764
KevinClauson <kevinclauson99@gmail.com>
2017-10-31 22:51:51 +0000

    safe free

---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * free_listint_safe - prints a listint_t linked list and prevents looping.
+ * @h: a pointer to a pointer to a linked list.
+ * Return: number of nodes freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp, *temp2;
+	size_t cnt;
+
+	cnt = 0;
+	if (*h == NULL)
+		return (cnt);
+        temp = *h;
+	while (temp != NULL)
+	{
+		temp2 = temp;
+		temp = temp->next;
+		temp2->next = NULL;
+		free(temp2);
+		++cnt;
+	}
+	*h = NULL;
+	return (cnt);
+}
