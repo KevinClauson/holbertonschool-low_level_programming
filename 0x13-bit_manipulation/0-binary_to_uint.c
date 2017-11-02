86acd7b5dd09638116362b341b4b4457b79073b1
KevinClauson <kevinclauson99@gmail.com>
2017-11-02 20:15:34 +0000

    catch when string is longer than long int

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * _strlen - calc length of string.
- * @str get string length.
+ * @str: get string length.
  * Return: length of string.
  */
 
@@ -22,10 +22,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, num;
 	int len;
-
+/*
 	len = _strlen(b);
 	if (len > 66)
 		return (0);
+*/
 	num = 0;
 	for (i = 0; b[i] != '\0'; ++i)
 	{
