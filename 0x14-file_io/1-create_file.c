35d7990fc0f876ff51bf0f584985c4d4cc149130
KevinClauson <kevinclauson99@gmail.com>
2017-11-08 02:13:29 +0000

    truncate file if alread exists

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fopen = open(filename, O_CREAT | O_WRONLY, 0600);
+	fopen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fopen == -1)
 		return (-1);
 	if (text_content != NULL)
