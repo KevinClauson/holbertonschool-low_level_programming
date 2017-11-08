80e0db352141fada1b5ca05aabb23ffecb72fc29
KevinClauson <kevinclauson99@gmail.com>
2017-11-08 20:51:48 +0000

    cp project

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#define BUFFSIZE 1024
+/**
+ * main - program that copies the content of a file to another file.
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+	int fd1, fd2, fd_r, fd_w;
+	char *f_fr, *f_to, buffer[BUFFSIZE];
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f_fr = argv[1];
+	f_to = argv[2];
+	fd1 = open(f_fr, O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_fr);
+		exit(98);
+	}
+	fd2 = open(f_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_to);
+		exit(99);
+	}
+
+	do {
+		fd_r = read(fd1, &buffer, BUFFSIZE);
+		if (fd_r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", f_fr);
+			close(fd1);
+			close(fd2);
+			exit(98);
+		}
+		fd_w = write(fd2, &buffer, fd_r);
+		if (fd_w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f_to);
+			close(fd1);
+			close(fd2);
+			exit(99);
+		}
+	} while (fd_r > 0);
+
+	return (0);
+}
