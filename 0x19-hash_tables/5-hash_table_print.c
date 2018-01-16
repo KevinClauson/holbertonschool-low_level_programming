46ff7caeb020303e72cafbed7fdd7924a5c2e359
KevinClauson <kevinclauson99@gmail.com>
2018-01-16 19:49:31 +0000

    took oout if check for element in arra

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -16,20 +16,17 @@ void hash_table_print(const hash_table_t *ht)
 		printf("{");
 		for (i = 0, flag = 0; i < ht->size; ++i)
 		{
-			if (ht->array[i])
+			temp = ht->array[i];
+			while (temp)
 			{
-				temp = ht->array[i];
-				while (temp)
+				if (temp->key && temp->value)
 				{
-					if (temp->key && temp->value)
-					{
-						if (flag == 1)
-							printf(", ");
-						printf("'%s': '%s'", temp->key, temp->value);
-						flag = 1;
-					}
-					temp = temp->next;
+					if (flag == 1)
+						printf(", ");
+					printf("'%s': '%s'", temp->key, temp->value);
+					flag = 1;
 				}
+				temp = temp->next;
 			}
 		}
 		printf("}\n");
