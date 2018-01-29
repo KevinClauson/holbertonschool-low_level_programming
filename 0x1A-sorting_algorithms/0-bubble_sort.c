f719cb9e892897cdd8afc6fe767330b7f5d6f66b
KevinClauson <kevinclauson99@gmail.com>
2018-01-29 20:35:35 +0000

    style and null edits

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,30 +1,47 @@
 #include "sort.h"
 
+/**
+ * swap - swap the elements
+ * @a: element to be swapped
+ * @b: other elementt to be swapped
+ *
+ * Return: Nothing
+ */
 void swap(int *a, int *b)
 {
-    *a ^= *b;
-    *b ^= *a;
-    *a ^= *b;
+	*a ^= *b;
+	*b ^= *a;
+	*a ^= *b;
 }
 
+/**
+ * bubble_sort - sort array
+ * @array: array to be sorted
+ * @size: length of array to be sorted
+ *
+ * Return: Nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int flag;
+	size_t i, j;
+	int flag;
 
-    for (i = 0; i < size - 1; ++i)
-    {
-        flag = 1;
-        for (j = 0; j < size - i - 1; ++j)
-        {
-            if (array[j] > array[j+1])
-            {
-                swap(&array[j], &array[j+1]);     
-                print_array(array, size);
-                flag = 0;
-            }
-        }
-        if (flag)
-            break;
-    }
+	if (array)
+	{
+		for (i = 0; i < size - 1; ++i)
+		{
+			flag = 1;
+			for (j = 0; j < size - i - 1; ++j)
+			{
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+					flag = 0;
+				}
+			}
+			if (flag)
+				break;
+		}
+	}
 }
