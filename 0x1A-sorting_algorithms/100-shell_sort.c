be01592c09e919a457b5309118e9159f2bb15a6c
KevinClauson <kevinclauson99@gmail.com>
2018-01-30 06:09:59 +0000

    shell sort

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+
+/**
+ * shell_sort - sort the array
+ *
+ * @array: array
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j, num, len;
+	int temp;
+
+	len = 1;
+	if (size % 3 == 0)
+		num = size / 3;
+	else
+		num = (size / 3) + 1;
+	while (len < num)
+		len *= (len * 3) + 1;
+	while (len >= 1)
+	{
+		for (i = len; i < size; i += 1)
+		{
+			temp = array[i];
+
+			for (j = i; j >= len && array[j - len] > temp; j -= len)
+			{
+				array[j] = array[j - len];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+		len /= 3;
+	}
+}
