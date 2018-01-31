50fe3312405f448163cca9c0164cac1cb29960dc
KevinClauson <kevinclauson99@gmail.com>
2018-01-31 06:57:16 +0000

    style

--- 0x1A-sorting_algorithms/101-cocktail_sort_list.c
+++ 0x1A-sorting_algorithms/101-cocktail_sort_list.c
@@ -9,78 +9,65 @@
  */
 void cocktail_sort_list(listint_t **list)
 {
-	if (list && *list && (*list)->next)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	int flag;
+	listint_t *temp, *c = *list;
+
+	flag = 1;
+	while (flag)
 	{
-		int flag;
-		listint_t *c, *temp;
-
-		flag = 1;
-		c = *list;
-
-		while (flag)
+		flag = 0;
+		while (c->next)
 		{
-			flag = 0;
-			while(c->next)
+			if (c->n > c->next->n)
 			{
-				if (c->n > c->next->n)
+				if (c->prev == NULL)
 				{
-					if (c->prev == NULL)
-					{
-						c->next->prev = NULL;
-						*list = c->next;
-						c->prev = c->next;
-						temp = c->next;
-						c->next = c->next->next;
-						temp->next = c;
-						flag = 1;
-						print_list(*list);
-					}
-					else
-					{
-						temp = c->next;
-						temp->prev = c->prev;
-						c->prev->next = temp;
-						if (temp->next)
-						{
-							temp->next->prev = c;
-						}
-						c->next = temp->next;
-						temp->next = c;
-						c->prev = temp;
-						print_list(*list);
-						flag = 1;
-					}
-				}
-				else
-				{
-					c = c->next;
-				}
-			}
-			while(c->prev)
-			{
-				if (c->n < c->prev->n)
-				{
-					temp = c->prev;
-					c->prev = temp->prev;
-					c->next->prev = temp;
-					temp->next = c->next;
-					c->next = temp;
-					temp->prev = c;
-					flag = 1;
-					if (c->prev)
-						c->prev->next = c;
-					else
-					{
-						*list = c;
-					}
+					c->next->prev = NULL;
+					*list = c->next;
+					c->prev = c->next;
+					temp = c->next;
+					c->next = c->next->next;
+					temp->next = c;
 					print_list(*list);
 				}
 				else
 				{
-					c = c->prev;
+					temp = c->next;
+					temp->prev = c->prev;
+					c->prev->next = temp;
+					if (temp->next)
+						temp->next->prev = c;
+					c->next = temp->next;
+					temp->next = c;
+					c->prev = temp;
+					print_list(*list);
 				}
+				flag = 1;
 			}
-
+			else
+				c = c->next;
+		}
+		while (c->prev)
+		{
+			if (c->n < c->prev->n)
+			{
+				temp = c->prev;
+				c->prev = temp->prev;
+				c->next->prev = temp;
+				temp->next = c->next;
+				c->next = temp;
+				temp->prev = c;
+				flag = 1;
+				if (c->prev)
+					c->prev->next = c;
+				else
+					*list = c;
+				print_list(*list);
+			}
+			else
+				c = c->prev;
 		}
 	}
 }
