b342367d63a949367cc0ce3a068ce32621e9ca55
KevinClauson <kevinclauson99@gmail.com>
2018-02-17 07:01:58 +0000

    add when single line

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -15,15 +15,15 @@ def island_perimeter(grid):
                 if i == 0:
                     if j == 0:
                         perm += 2
-                        if grid[i][j+1] == 0:
+                        if j == len(line) - 1 or grid[i][j+1] == 0:
                             perm += 1
-                        if grid[i+1][j] == 0:
+                        if i == len(grid) - 1 or grid[i+1][j] == 0:
                             perm += 1
                     elif j == len(line) - 1:
                         perm += 2
-                        if grid[i][j-1] == 0:
+                        if j == 0 or grid[i][j-1] == 0:
                             perm += 1
-                        if grid[i-1][j] == 0:
+                        if i == 0 or grid[i-1][j] == 0:
                             perm += 1
                     else:
                         perm += 1
@@ -36,13 +36,13 @@ def island_perimeter(grid):
                 elif i == len(grid) - 1:
                     if j == 0:
                         perm += 2
-                        if grid[i-1][j] == 0:
+                        if i == 0 or grid[i-1][j] == 0:
                             perm += 1
-                        if grid[i][j+1] == 0:
+                        if j == len(line) - 1 or grid[i][j+1] == 0:
                             perm += 1
                     elif j == len(line) - 1:
                         perm += 2
-                        if grid[i-1] == 0:
+                        if grid[i-1][j] == 0:
                             perm += 1
                         if grid[i][j-1] == 0:
                             perm += 1
