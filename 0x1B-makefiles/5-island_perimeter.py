4bc731735c077aeda65e796c3b5599022fa9eb80
KevinClauson <kevinclauson99@gmail.com>
2018-02-17 07:45:34 +0000

    correct logic

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -21,9 +21,9 @@ def island_perimeter(grid):
                             perm += 1
                     elif j == len(line) - 1:
                         perm += 2
-                        if j == 0 or grid[i][j-1] == 0:
+                        if grid[i][j-1] == 0:
                             perm += 1
-                        if i == 0 or grid[i-1][j] == 0:
+                        if i == len(grid) - 1 or grid[i-1][j] == 0:
                             perm += 1
                     else:
                         perm += 1
@@ -31,12 +31,12 @@ def island_perimeter(grid):
                             perm += 1
                         if grid[i][j+1] == 0:
                             perm += 1
-                        if grid[i-1][j] == 0:
+                        if i == len(grid) - 1 or grid[i+1][j] == 0:
                             perm += 1
                 elif i == len(grid) - 1:
                     if j == 0:
                         perm += 2
-                        if i == 0 or grid[i-1][j] == 0:
+                        if grid[i-1][j] == 0:
                             perm += 1
                         if j == len(line) - 1 or grid[i][j+1] == 0:
                             perm += 1
@@ -57,7 +57,7 @@ def island_perimeter(grid):
                 else:
                     if j == 0:
                         perm += 1
-                        if grid[i][j+1] == 0:
+                        if j == len(line) - 1 or grid[i][j+1] == 0:
                             perm += 1
                         if grid[i-1][j] == 0:
                             perm += 1
