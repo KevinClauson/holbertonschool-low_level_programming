ce367a5298b1dcfdf1a4631540eec94abac113ea
KevinClauson <kevinclauson99@gmail.com>
2018-03-07 03:14:45 +0000

    edit 2 funcs into one

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -1,28 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * rec_size - recursive functiont that returns size
- * @tree: node to tree;
- * @size: size of tree;
- * Return: returns size of tree;
- */
-size_t rec_size(const binary_tree_t *tree, size_t size)
-{
-	if (!tree)
-		return 0;
-	return 1 + rec_size(tree->left, size) + rec_size(tree->right, size);
-}
-
-
-/**
  * binary_tree_depth - measures the depth of a node in a binary tree
  * @node is a pointer to the node to measure the depth
  * Return: depth as a size_t or 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size;
+	if (tree == NULL)
+		return 0;
 
-	size = 0;
-	return rec_size(tree, size);
+	return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 }
