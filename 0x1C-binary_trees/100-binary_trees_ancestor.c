9877cdbeff54fd8356f7e5c680ec31254dca322e
KevinClauson <kevinclauson99@gmail.com>
2018-03-07 08:32:29 +0000

    add 100

---
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_qncestor - finds common ancestor of 2 nodes
+ * @firsst: is a pointer to a node
+ * @second: is a pointer to a node
+ * Return: ancestor or Null if none
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	const binary_tree_t *temp;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	while (first != NULL)
+	{
+		temp = second;
+		while (temp != NULL)
+		{
+			if (first == temp)
+				return ((binary_tree_t *) temp);
+			temp = temp->parent;
+		}
+		first = first->parent;
+	}
+	return (NULL);
+}
