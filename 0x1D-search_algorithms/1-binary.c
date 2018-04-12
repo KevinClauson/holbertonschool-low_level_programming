ddd7e950dc8fac6c3f181b11373d820bfe9ab682
KevinClauson <kevinclauson99@gmail.com>
2018-04-12 18:21:41 +0000

    style changes

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,33 +1,36 @@
 #include "search_algos.h"
 /**
- * main - Entry point
- *
- **/
- int binary_search(int *array, size_t size, int value)
+* binary_search - Entry point
+* @array: array of ints that are sorted
+* @size: size of array
+* @value: what to search for
+**/
+int binary_search(int *array, size_t size, int value)
 {
   size_t start, end, mid, i;
+  
+  start = 0;
+  end = size - 1;
+  mid = (start + end) / 2;
+  while(start <= end)
+    {
+	  		printf("Searching in array:");
+	  		for (i = start; i <= end; ++i)
+	  		{
+	    			printf(" %d", array[i]);
+	    			if (i < end)
+	      				printf(",");
+	    			else
+	      				printf("\n");
+	  		}
+			if (array[mid] == value)
+			  return(mid); 
 
-      start = 0;
-      end = size - 1;
-      mid = (start + end) / 2;
-      while(start <= end)
-      {
-	  if (array[mid] == value)
-	    return(mid); 
-	  printf("Searching in array:");
-	  for (i = start; i <= end; ++i)
-	  {
-	    printf(" %d", array[i]);
-	    if (i < end)
-	      printf(",");
-	    else
-	      printf("\n");
-	  }
-	if (array[mid] < value)
-	  start = mid+1;
-	else if (array[mid] > value)
-	  end = mid-1;
-	mid = (start + end) / 2;
-      }
-      return (-1);
+			if (array[mid] < value)
+			  start = mid+1;
+			else if (array[mid] > value)
+			  end = mid-1;
+		        mid = (start + end) / 2;
+		}
+		return (-1);
 }
